065ac590594cfc2588190a3820bf5f4ec80f65a3
NJ <monu.bits@gmail.com>
2023-12-03 16:25:28 +0530

    updated styles

--- src/frontend/Login.js
+++ src/frontend/Login.js
@@ -2,27 +2,26 @@ const Login = ({ username, password, setUsername, setPassword, handleLogin, logi
   return (
     <div className="login">
       <div className="panel">
-        <div className="credential">
-          <input
-            type="text"
-            value={username}
-            placeholder="enter username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            value={password}
-            placeholder="enter password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+        <input
+          type="text"
+          value={username}
+          placeholder="enter username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="enter password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button
           disabled={!username.length || !password.length}
           onClick={() => handleLogin()}
         >Login</button>
-        {loginError && (
-          <div className="login-error">Incorrect Username or Password!</div>
-        )}
+        <div
+          className="login-error"
+          style={{ color: loginError ? "red" : "transparent" }}
+        >Incorrect Username or Password!</div>
       </div>
     </div>
   )
